55dae89176d6462425fe2898d50854aa8b20ad3a
rajeshwari krishnamurthy <rk633@afsconnect1.njit.edu>
2017-10-04 17:51:47 -0400

    Fourth commit - Searching and finding position of '/'

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$date =  date('Y/m/d', time());
+$date =  date('Y-m-d', time());
 echo "The value of \$date: ".$date."<br>";
 
 $tar = "2017/05/24";
@@ -7,14 +7,27 @@ echo "The value of \$tar: ".$tar."<br>";
 
 $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
-print_r($year)
+print_r($year);
+echo "</br><br>";
 
+echo "Replacing - with /</br>";
+$date =  date('Y/m/d', time());
+echo "The value of \$date: ".$date."<br><br>";
+
+echo " Comparing $date and $tar<br>";
 if(strcmp($date,$tar)>0)
   { echo "The future";
 }else if(strcmp($date,$tar)<0)
-  { echo "The past";
+{ echo "The past";
 }else
-  { echo "Oops";
+{ echo "Oops";
 }
+echo "<br><br>";
 
+echo " Searching for '/' in $date<br>";
+$pos = 0;
+while ($pos > -1) {
+$pos = strpos($date, "/",$pos+1);
+echo "  " .$pos. "  ";
+}
 ?>
